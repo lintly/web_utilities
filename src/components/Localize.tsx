75cde35a811547eedcb9588296c1e1a947f637ae
Jeff Miller <lintly@Lintly-MacBook-Pro-5.local>
2025-04-04 13:36:35 -0500

    Update to better handle localize

--- src/components/Localize.tsx
+++ src/components/Localize.tsx
@@ -1,62 +1,75 @@
 import { SetStateAction, useState } from "react";
-import { localize } from 'pseudo-localization'; // https://github.com/tryggvigy/pseudo-localization/tree/master
+import { localize } from "pseudo-localization"; // https://github.com/tryggvigy/pseudo-localization/tree/master
 import copyIcon from "/copy-icon.svg";
 
 const Localize = () => {
-    const [plainText, setPlainText] = useState<string>("");
-    const [localText, setLocalText] = useState<string>("");
+  const [plainText, setPlainText] = useState<string>("");
+  const [localText, setLocalText] = useState<string>("");
+  const [status, setStatus] = useState<string>("");
 
-    const handlePlainChange = (event: {
-        target: { value: SetStateAction<string> };
-    }) => {
-        setPlainText(event.target.value);
-    };
+  const copyLocalizedText = (text: string) => {
+    navigator.clipboard.writeText(text);
+    setStatus("Localized text copied to clipboard!");
+    setTimeout(() => {
+      setStatus("");
+    }, 3000);
+  };
 
-    const localIt = () => {
-        setLocalText(localize(plainText));
-    };
+  const handlePlainChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setPlainText(event.target.value);
+  };
 
-    const clearAll = () => {
-        setPlainText("");
-        setLocalText("");
-    };
+  const localIt = () => {
+    const text = localize(plainText);
+    setLocalText(text);
+    copyLocalizedText(text);
+  };
 
-    return (
-        <div>
-            <h1>PsuedoLocalization</h1>
-            <div className="flexBox">
-                <div style={{ width: "50%" }}>
-                    <h2>
-                        String To Localize
-                    </h2>
-                    <textarea
-                        style={{ width: "100%", height: "200px" }}
-                        value={plainText}
-                        onChange={handlePlainChange}
-                    />
-                </div>
-                <div style={{ width: "50%"  }}>
-                    <h2>
-                        Localized String&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button
-                            className="copyButton"
-                            onClick={() =>
-                                navigator.clipboard.writeText(localText)
-                            }
-                        >
-                            <img src={copyIcon} className="copyImg" />
-                        </button>
-                    </h2>
-                    <textarea
-                        style={{ width: "100%", height: "200px" }}
-                        value={localText}
-                    />
-                </div>
-            </div>
-            <button onClick={() => localIt()}>Localize Me!</button>
-            <button className="clear-all-button" onClick={() => clearAll()}>Clear All</button>
+  const clearAll = () => {
+    setPlainText("");
+    setLocalText("");
+  };
+
+  return (
+    <div>
+      <h1>PsuedoLocalization</h1>
+      <div className="flexBox">
+        <div style={{ width: "50%" }}>
+          <h2>String To Localize</h2>
+          <textarea
+            style={{ width: "100%", height: "200px" }}
+            value={plainText}
+            onChange={handlePlainChange}
+            onFocus={() => setPlainText("")}
+          />
         </div>
-    );
+        <div style={{ width: "50%" }}>
+          <h2>
+            Localized String&nbsp;&nbsp;&nbsp;&nbsp;
+            <button
+              className="copyButton"
+              onClick={() => copyLocalizedText(localText)}
+            >
+              <img src={copyIcon} className="copyImg" />
+            </button>
+          </h2>
+          <textarea
+            style={{ width: "100%", height: "200px" }}
+            value={localText}
+          />
+        </div>
+      </div>
+      {status && (
+        <span style={{ color: "red", fontWeight: "bold" }}>{status}</span>
+      )}
+      <button onClick={() => localIt()}>Localize Me!</button>
+      <button className="clear-all-button" onClick={() => clearAll()}>
+        Clear All
+      </button>
+    </div>
+  );
 };
 
 export default Localize;
