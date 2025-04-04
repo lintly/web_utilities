0b0211c8ebf8b4f779edbaf0ce9af43576c41859
Jeff Miller <lintly@gmail.com>
2025-04-04 14:15:34 -0500

    Add colorizer (#2)
    Co-authored-by: Jeff Miller <lintly@Lintly-MacBook-Pro-5.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home';
-import Base64 from './components/Base64';
-import Localize from './components/Localize';
-import JWT from './components/JWT';
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Base64 from "./components/Base64";
+import Localize from "./components/Localize";
+import JWT from "./components/JWT";
+import Colorizer from "./components/Colorizer";
 
 enum sections {
   "home",
   "base64",
   "local",
-  "jwt"
+  "jwt",
+  "color",
 }
 
 function App() {
@@ -17,18 +19,45 @@ function App() {
 
   return (
     <>
-      <div className='header'>
-        <button className='menu-button' onClick={() => setSection(sections.home)}>Home</button>
-        <button className='menu-button' onClick={() => setSection(sections.base64)}>Base64 Encode/Decode</button>
-        <button className='menu-button' onClick={() => setSection(sections.jwt)}>JWT Decode</button>
-        <button className='menu-button' onClick={() => setSection(sections.local)}>PsuedoLocalization Generator</button>
+      <div className="header">
+        <button
+          className="menu-button"
+          onClick={() => setSection(sections.home)}
+        >
+          Home
+        </button>
+        <button
+          className="menu-button"
+          onClick={() => setSection(sections.base64)}
+        >
+          Base64 Encode/Decode
+        </button>
+        <button
+          className="menu-button"
+          onClick={() => setSection(sections.jwt)}
+        >
+          JWT Decode
+        </button>
+        <button
+          className="menu-button"
+          onClick={() => setSection(sections.local)}
+        >
+          PsuedoLocalization Generator
+        </button>
+        <button
+          className="menu-button"
+          onClick={() => setSection(sections.color)}
+        >
+          Colorizer
+        </button>
       </div>
       {section === sections.home && <Home />}
       {section === sections.base64 && <Base64 />}
       {section === sections.local && <Localize />}
       {section === sections.jwt && <JWT />}
+      {section === sections.color && <Colorizer />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/JWT.tsx
+++ src/components/JWT.tsx
@@ -3,81 +3,79 @@ import copyIcon from "/copy-icon.svg";
 import * as jose from "jose";
 
 const JWT = () => {
-    const [jwtToken, setJwtToken] = useState<string>("");
-    const [headerText, setHeaderText] = useState<string>("");
-    const [dataText, setDataText] = useState<string>("");
+  const [jwtToken, setJwtToken] = useState<string>("");
+  const [headerText, setHeaderText] = useState<string>("");
+  const [dataText, setDataText] = useState<string>("");
 
-    const handlePlainChange = (event: {
-        target: { value: SetStateAction<string> };
-    }) => {
-        setJwtToken(event.target.value);
-    };
+  const handlePlainChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setJwtToken(event.target.value);
+  };
 
-    const decodeJWT = async () => {
-        if (jwtToken) {
-            const protectedHeader = jose.decodeProtectedHeader(jwtToken);
-            setHeaderText(JSON.stringify(protectedHeader, null, 2));
+  const decodeJWT = async () => {
+    if (jwtToken) {
+      const protectedHeader = jose.decodeProtectedHeader(jwtToken);
+      setHeaderText(JSON.stringify(protectedHeader, null, 2));
 
-            const decodedSub = jose.decodeJwt(jwtToken);
-            setDataText(JSON.stringify(decodedSub, null, 2));
-        }
-    };
+      const decodedSub = jose.decodeJwt(jwtToken);
+      setDataText(JSON.stringify(decodedSub, null, 2));
+    }
+  };
 
-    const clearAll = () => {
-        setJwtToken("");
-        setHeaderText("");
-    };
+  const clearAll = () => {
+    setJwtToken("");
+    setHeaderText("");
+  };
 
-    return (
-        <>
-            <h1>JWT Decode</h1>
-            <div className="flexBox">
-                <div style={{ width: "50%" }}>
-                    <h2>Encoded JWT</h2>
-                    <textarea
-                        style={{ width: "100%", height: "567px" }}
-                        value={jwtToken}
-                        onChange={handlePlainChange}
-                    />
-                </div>
-                <div style={{ width: "50%" }}>
-                    <h2>
-                        Decode
-                    </h2>
-                    <h3>Header&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button
-                            className="copyButton"
-                            onClick={() =>
-                                navigator.clipboard.writeText(headerText)
-                            }
-                        >
-                            <img src={copyIcon} className="copyImg" />
-                        </button></h3>
-                    <textarea
-                        style={{ width: "100%", height: "200px" }}
-                        value={headerText}
-                    />
-                    <h3>Payload&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button
-                            className="copyButton"
-                            onClick={() =>
-                                navigator.clipboard.writeText(dataText)
-                            }
-                        >
-                            <img src={copyIcon} className="copyImg" />
-                        </button></h3>
-                    <textarea
-                        style={{ width: "100%", height: "200px" }}
-                        value={dataText}
-                    />
-                </div>
-            </div>
-            <button onClick={() => decodeJWT()}>Decode JWT</button>
-            <button className="clear-all-button" onClick={() => clearAll()}>
-                Clear All
+  return (
+    <>
+      <h1>JWT Decode</h1>
+      <div className="flexBox">
+        <div style={{ width: "50%" }}>
+          <h2>Encoded JWT</h2>
+          <textarea
+            style={{ width: "100%", height: "567px", fontSize: "16px" }}
+            value={jwtToken}
+            onChange={handlePlainChange}
+          />
+        </div>
+        <div style={{ width: "50%" }}>
+          <h2>Decode</h2>
+          <h3>
+            Header&nbsp;&nbsp;&nbsp;&nbsp;
+            <button
+              className="copyButton"
+              onClick={() => navigator.clipboard.writeText(headerText)}
+            >
+              <img src={copyIcon} className="copyImg" />
             </button>
-        </>
-    );
+          </h3>
+          <textarea
+            style={{ width: "100%", height: "200px", fontSize: "16px" }}
+            value={headerText}
+          />
+          <h3>
+            Payload&nbsp;&nbsp;&nbsp;&nbsp;
+            <button
+              className="copyButton"
+              onClick={() => navigator.clipboard.writeText(dataText)}
+            >
+              <img src={copyIcon} className="copyImg" />
+            </button>
+          </h3>
+          <textarea
+            style={{ width: "100%", height: "200px", fontSize: "16px" }}
+            value={dataText}
+          />
+        </div>
+      </div>
+      <button onClick={() => decodeJWT()}>Decode JWT</button>
+      <button className="clear-all-button" onClick={() => clearAll()}>
+        Clear All
+      </button>
+    </>
+  );
 };
 
 export default JWT;
